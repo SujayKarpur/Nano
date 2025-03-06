081d706687576bc3463e0e261b6d92f6285df98e
SujayKarpur <PES1UG23CS607@pesu.pes.edu>
2025-03-06 06:22:20 +0000

    Add main file

---
+++ database.py
@@ -0,0 +1,13 @@
+class Database:
+
+    def __init__(self, ):
+        pass 
+
+    def set(self, key, value):
+        pass 
+
+    def get(self, key):
+        pass 
+
+    def delete(self, key):
+        pass 

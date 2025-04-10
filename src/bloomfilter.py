68f2f8fd2c73540e3aab209ef558311fc4258ec2
SujayKarpur <PES1UG23CS607@pesu.pes.edu>
2025-04-10 07:50:18 +0000

    Add requirements.txt

---
+++ src/bloomfilter.py
@@ -0,0 +1,18 @@
+import hashlib 
+from bitarray import bitarray
+import math 
+
+
+class BloomFilter:
+
+    def __init__(self, n, k):
+        pass 
+
+    def insert(self, ): 
+        pass 
+
+    def __contains__(self, item):
+        pass 
+
+    def __repr__(self) -> str:
+        pass 

36a44291f88747421b3e051720aaed6c3b2e902a
SujayKarpur <PES1UG23CS607@pesu.pes.edu>
2025-04-11 11:22:26 +0000

    Define LSM tree interface

--- src/lsm.py
+++ src/lsm.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 from src.redblacktree import RedBlackTree 
 from src.wal import WAL 
 from src.bloomfilter import BloomFilter
@@ -10,10 +13,25 @@ from src.sstable import SSTable
 class LSMTree:
 
     def __init__(self, name: str) -> None:
+        self.name = name  
+        self.memtable = Memtable(name)
+        self.sstable = SSTable(name)
+
+    
+    def startup(self) -> None:
         pass 
 
-    def flush(self, ):
+    def shutdown(self) -> None:
         pass 
 
-    def compact():
-        pass #idk
+
+    def set(self, key: str, value: str) -> bool: 
+        pass 
+
+
+    def get(self, key: str) -> Tuple[bool,str]:
+        pass 
+
+
+    def delete(self, key: str) -> bool:
+        pass  
--- src/sstable.py
+++ src/sstable.py
@@ -11,45 +11,6 @@ from env import PATH, FLUSH_SIZE
 
 
 
-"""class MetaBlock:
-
-    def __init__(self, name: str, count: int, data: Memtable) -> None:
-        self.data = data 
-        self.name = name 
-        self.count = count 
-        self.file_name = f'{PATH}/storage/{name}/sstable_metablock_{count}' 
-
-        bf = BloomFilter()
-
-        ol = data.ordered_list()
-        min_key = ol[0]; max_key = ol[-1]
-
-        for node in ol:
-            bf.insert(node.key)
-
-
-        with open(self.file_name, 'w') as f:
-            print(bf, file=f)
-            print(min_key, max_key, file=f)
-            print(data.number_of_elements, file=f)
-
-
-
-
-    def find(self, key: str) -> Tuple[bool,str]:
-        
-        with open(self.file_name, 'r') as f:
-            contents = [i.rstrip('\n') for i in f.readlines()]
-            filter_string = contents[0]
-            bf = BloomFilter(filter_string)
-            if key not in bf:
-                return (False, '')
-            min_key, max_key = contents[1].split()
-            if key < min_key or key > max_key:
-                return False 
-            n = int(contents[2])
-            return #search in the corresponding data file """
-
 
 
 class SSTable:
@@ -151,10 +112,6 @@ class SSTable:
 
 
 
-    def flush(self, ) -> None:
+    def compact(self) -> None:
         pass 
-
-    def get(self, key: str) -> None:
-        pass 
-
     
